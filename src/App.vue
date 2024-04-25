2d020a07860d681b64866d488269d69185ee308e
OutlawDev <siyabonga.ncube669@gmail.com>
2024-04-25 11:32:38 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ <div class="landing-page">
+    <h1>Nail Treatment</h1>
+    <p>Welcome to the SALON for your nails</p>
+ </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+ name: 'App',
+});
+</script>
+
+<style scoped>
+.landing-page {
+ text-align: center;
+ padding: 2rem;
+}
+</style>
